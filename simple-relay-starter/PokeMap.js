bebd952f81b4d2b17f04447548f33db7280534c1
Artur Balestro <artur.balestro@objectedge.com>
2016-12-07 18:23:34 -0200

    A few changes, including defining game completion status and prizes for winning.

--- simple-relay-starter/PokeMap.js
+++ simple-relay-starter/PokeMap.js
@@ -4,6 +4,9 @@ var ReactDOM = require('react-dom')
 var Relay = require('react-relay')
 import { Label, ButtonToolbar, ButtonGroup, Button, Grid, Row, Col, Image, Modal } from 'react-bootstrap';
 
+const pairsFound = [];
+const tokenInventory = [];
+
 class PokeMap extends React.Component {
   constructor(props) {
     super(props);
@@ -29,7 +32,7 @@ class PokeMap extends React.Component {
 
     //Step 2: Select a group of random pokemon to appear in the tiles
     const tileGroup = [];
-    for(let i = 0; i < 21; i++) {
+    for(let i = 0; i < 7; i++) {
       const randomPokemon = this.randomNumber(availablePokemon.length);
       const chosenPokemon = availablePokemon[randomPokemon - 1];
       tileGroup.push(chosenPokemon.node);
@@ -74,15 +77,27 @@ class Tile extends React.Component {
 
     this.state = {
       tileVisible: false,
+      gameCompleted: false,
+      showModal: false,
+      lastFound: {},
     };
 
     this.revealTile = this.revealTile.bind(this);
     this.unrevealTile = this.unrevealTile.bind(this);
+    this.closeModal = this.closeModal.bind(this);
+    this.openModal = this.openModal.bind(this);
+  }
+
+  closeModal() {
+    this.setState({ showModal: false });
+  }
+
+  openModal() {
+    this.setState({ showModal: true });
   }
 
   revealTile(e) {
     this.setState({ tileVisible: true });
-    console.log('pokebgs...', document.getElementsByClassName('pokebg'));
     e.target.classList.add('activeTile');
 
     const activeTile = document.getElementsByClassName('activeTile');
@@ -93,8 +108,11 @@ class Tile extends React.Component {
     }
   }
 
-  unrevealTile() {
-    this.setState({ tileVisible: false });
+  unrevealTile(tiles) {
+    setTimeout(function() {
+      tiles[0].classList.remove('activeTile');
+      tiles[0].classList.remove('activeTile');
+    },500);
   }
 
   checkPair(tiles) {
@@ -102,24 +120,39 @@ class Tile extends React.Component {
 
     if(tiles[0].alt == tiles[1].alt) {
       console.log('You got a pair!');
+      pairsFound.push(tiles[0]);
+      console.log('pairsFound for: ', pairsFound);
+
       tiles[0].classList.add('correctTile');
       tiles[1].classList.add('correctTile');
       tiles[0].classList.remove('activeTile');
       tiles[0].classList.remove('activeTile');
+
+      this.checkCompletion(pairsFound);
     }else{
-      console.log('--------', tiles[0].classList, tiles[1].classList);
-      setTimeout(function() {
-        tiles[0].classList.remove('activeTile');
-        tiles[0].classList.remove('activeTile');
-      },500);
+      this.unrevealTile(tiles);
+    }
+  }
+
+  checkCompletion(pairsFound) {
+    const tiles = document.getElementsByClassName('pokebg');
+    console.log('last pair found?', pairsFound.slice(-1)[0]);
+    const lastFound = pairsFound.slice(-1)[0];
+    /*You should get a token correspondent to the last pokemon pair you found.
+    A number of tokens can unlock the evolution of this pokemon, and some amount of tokens can unlock different and rarer pokemon.
+    Also, as the game progresses the level of difficulty increases a bit (by adding more tiles and possibly other twists).*/
+
+    if(pairsFound.length == tiles.length / 2) {
+      console.log('congrats, you completed the game! Modal with the prize should be opened soon!');
+      this.setState({ gameCompleted: true, lastFound: lastFound });
     }
   }
 
   render() {
-    //console.log(this.state);
     const grassImage = '/img/grass.jpg';
-    var chosenPokemon = this.props.chosenPokemon;
+    const chosenPokemon = this.props.chosenPokemon;
     //console.log('A wild ', chosenPokemon.name, ' appeared!');
+    console.log('states: ', this.state);
     return (
       <div className="poketile">
         <Image
@@ -132,7 +165,56 @@ class Tile extends React.Component {
         {/* {this.state.tileVisible && */}
           <Image className="pokeimg" src={chosenPokemon.image} height="120" />
         {/* } */}
+        {this.state.gameCompleted &&
+          <PrizeModal prize={this.state.lastFound} showModal={true} />
+        }
       </div>
+    )
+  }
+}
+
+class PrizeModal extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      showModal: this.props.showModal,
+    };
+
+    this.closeModal = this.closeModal.bind(this);
+    //this.openModal = this.openModal.bind(this);
+  }
+
+  closeModal() {
+    this.setState({ showModal: false });
+  }
+
+  render() {
+    console.log('here is your prize ', this.props.prize);
+    //Need to pass the availablePokemon as props to this component.
+    /*const trainers = this.props.user.trainers.edges;
+    const wildGroup = trainers.filter(function(trainer) {
+      return trainer.node.name === "Wild";
+    });
+    const availablePokemon = wildGroup[0].node.pokemons.edges;
+    const prizePokemon = availablePokemon.filter(function(prize) {
+      return pokemon.node.name === this.props.prize.alt;
+    });
+    console.log('the token you will get is...', prizePokemon);*/
+
+    return (
+      <Modal show={this.state.showModal} onHide={this.closeModal}>
+        <Modal.Header closeButton>
+          <Modal.Title>Modal heading</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <h4>Congratulations, you found all the pairs!</h4>
+          <p>Received 1 Token.</p>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button onClick={this.closeModal}>Close</Button>
+        </Modal.Footer>
+      </Modal>
     )
   }
 }
