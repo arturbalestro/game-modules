3bd98d2a4e0a979c75ec3ebf7bc3c13aa873906c
Artur Balestro <artur.balestro@objectedge.com>
2017-02-16 18:00:58 -0200

    Attempting to readd the flip effect.

--- js/components/Tile.js
+++ js/components/Tile.js
@@ -46,19 +46,51 @@ export default class Tile extends React.Component {
     const tile = this.props.spot;
 
     return (
-      <div
-        className="poketile"
-        key={tile.id}
-        id={tile.pokemon.name}
-        onClick={this.props.selectTile.bind(this, tile)}
-      >
-        <Image
-          className="pokeimg"
-          src={tile.pokemon.image}
-          alt={tile.pokemon.name}
-          height="120"
-        />
-      </div>
+      // <div
+      //   className="poketile"
+      //   key={tile.id}
+      //   id={tile.pokemon.name}
+      //   onClick={this.props.selectTile.bind(this, tile)}
+      // >
+      //   <Image
+      //     className="pokeimg"
+      //     src={tile.pokemon.image}
+      //     alt={tile.pokemon.name}
+      //     height="120"
+      //   />
+      // </div>
+      // <div
+      //   className="poketile"
+      //   key={tile.id}
+      //   id={tile.pokemon.name}
+      //   onClick={this.props.selectTile.bind(this, tile)}
+      // >
+        <FlipCard
+          disabled={true}
+          flipped={this.state.isFlipped}
+        >
+          {/* The first child is used as the front of the card */}
+          <div className="poketile" onClick={this.showBack}>
+            <Image className="pokebg" src="img/tile-bg.jpg" onClick={this.showBack} />
+          </div>
+          {/* The second child is used as the back of the card */}
+          <div
+            className="poketile"
+            key={tile.id}
+            id={tile.pokemon.name}
+            // onClick={this.props.selectTile.bind(this, tile)}
+            onClick={this.showFront}
+          >
+            <Image
+              className="pokeimg"
+              src={tile.pokemon.image}
+              alt={tile.pokemon.name}
+              onClick={this.showFront}
+              height="120"
+            />
+          </div>
+        </FlipCard>
+      // </div>
     )
   }
 }
