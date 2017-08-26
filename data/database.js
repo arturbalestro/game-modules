a5700dd87ad16a58a48166691663eb27c486dbaf
arturbalestro <arturbalestro@hotmail.com>
2017-08-26 00:01:35 -0300

    ...

--- data/database.js
+++ data/database.js
@@ -1,43 +1,67 @@
 /**
- *  Copyright (c) 2015, Facebook, Inc.
- *  All rights reserved.
- *
- *  This source code is licensed under the BSD-style license found in the
- *  LICENSE file in the root directory of this source tree. An additional grant
- *  of patent rights can be found in the PATENTS file in the same directory.
- */
+*  Copyright (c) 2015, Facebook, Inc.
+*  All rights reserved.
+*
+*  This source code is licensed under the BSD-style license found in the
+*  LICENSE file in the root directory of this source tree. An additional grant
+*  of patent rights can be found in the PATENTS file in the same directory.
+*/
 
- // Model types
- export class Game {}
- export class Tile {}
+// Model types
+export class Game {}
+export class Tile {}
 
- // Mock data
- const game = new Game();
- game.id = '1';
+// Mock data
+const game = new Game();
+game.id = '1';
 
- const tiles = [];
- (function() {
-   for (let i = 1; i <= 9; i++) {
-     const rand = Math.ceil(Math.random() * 8);
-     console.log('picking up a random number from 1 to 9: ', rand);
-     const tile = new Tile();
-     tile.id = i;
-     tile.value = rand;
-     tile.hasBeenChecked = false;
-     tiles.push(tile);
-   }
-   console.log('tiles: ', tiles);
- })();
-
- let turnsRemaining = 8;
-
- export function checkTurns(id) {
-   turnsRemaining--;
+const tiles = [];
+(function() {
+ for (let i = 1; i <= 9; i++) {
+   const rand = Math.ceil(Math.random() * 8);
+   console.log('picking up a random number from 1 to 9: ', rand);
+   const tile = new Tile();
+   tile.id = i;
+   tile.value = rand;
+   tile.hasBeenChecked = false;
+   tiles.push(tile);
  }
- export function getTile(id) {
-   return tiles.find(t => t.id === id);
- }
+ console.log('tiles: ', tiles);
+})();
 
- export function getGame() { return game; }
- export function getTiles() { return tiles; }
- export function getTurnsRemaining() { return turnsRemaining; }
+const stages = [];
+export function Stage(id, question, answer, markedPosition) {
+  this.id = id
+  this.question = question
+  this.answer = answer
+  this.markedPosition = markedPosition
+}
+
+const stage1 = [
+  new Stage(1, 'Lacking in knowledge or training; unlearned.', 'IGNORANT', 1);
+  new Stage(2, 'An act of killing a lot of people.', 'MASSACRE', 1);
+  new Stage(3, 'A strong wish to achieve something.', 'AMBITION', 1);
+  new Stage(4, 'The act of going against loyalty.', 'BETRAYAL', 1);
+  new Stage(5, 'A person who watches what happens but has no active part in it.', 'OBSERVER', 0);
+  new Stage(6, 'Frightening.', 'FEARSOME', 0);
+  new Stage(7, 'A detailed plan for achieving success in situations such as war, politics, business, industry, or sport.', 'STRATEGY', 1);
+  new Stage(8, 'The crime of stealing things.', 'THIEVERY', 1);
+  new Stage(9, 'A royal title for a female heir to the throne.', 'PRINCESS', 1);
+  new Stage(10, 'To have control over a place or person.', 'DOMINATE', 1);
+  new Stage(11, 'A flat, usually square or rectangular, paper container for a letter.', 'ENVELOPE', 1);
+  new Stage(12, 'A set of formal acts, often fixed and traditional, performed on important social or religious occasions.', 'CEREMONY', 1);
+  new Stage(13, 'Accompanied by someone to a specific place.', 'ESCORTED', 1);
+];
+
+let turnsRemaining = 8;
+
+export function checkTurns(id) {
+ turnsRemaining--;
+}
+export function getTile(id) {
+ return tiles.find(t => t.id === id);
+}
+
+export function getGame() { return game; }
+export function getTiles() { return tiles; }
+export function getTurnsRemaining() { return turnsRemaining; }
--- js/components/App.js
+++ js/components/App.js
@@ -1,7 +1,19 @@
 import React from 'react';
 import Relay from 'react-relay';
 import TypedTransition from '../../scripts/TypedTransition';
-import { Row, Col, Nav, NavItem, Panel, Button, Glyphicon, Table } from 'react-bootstrap';
+import {
+  Row,
+  Col,
+  Nav,
+  NavItem,
+  Panel,
+  Button,
+  Glyphicon,
+  Table,
+  FormGroup,
+  FormControl,
+  ControlLabel
+} from 'react-bootstrap';
 
 class App extends React.Component {
   constructor(props) {
@@ -12,18 +24,287 @@ class App extends React.Component {
     console.log('----------numbers: ', this.props.game.tiles.edges);
     return (
       <Row className="text-center">
-        <Col md={1} sm={1} lg={1} xs={2} className="text-center">
-        <Table striped bordered condensed>
-          <tbody>
-            <tr>
-              {this.props.game.tiles.edges.map(edge =>
-                <td key={edge.node.id}>
-                  {edge.node.value}
-                </td>
-              )}
-            </tr>
-          </tbody>
-        </Table>
+        <Col md={12} sm={12} lg={12} xs={12} className="text-center">
+          <Panel header="Cryptogram">
+            <Table className="crypto-table" striped bordered condensed>
+              <tbody>
+                <tr>
+                  <td colSpan="16">
+                    Each number represents a different letter. When you have
+                    solved the Cryptogram, <b>the name of an epic Drama and
+                    Fantasy TV show that has been airing on HBO since 2011</b>
+                    will appear in the marked squares.
+                  </td>
+                </tr>
+                <tr>
+                  <td colSpan="8" className="text-left">
+                    Lacking in knowledge or
+                    training; unlearned.
+                  </td>
+                  <td className="tile">
+                    <FormGroup>
+                      <ControlLabel>1</ControlLabel>
+                      <FormControl type="text" />
+                      <FormControl type="hidden" value="I" />
+                    </FormGroup>
+                  </td>
+                  <td className="tile marked">
+                    <FormGroup>
+                      <FormControl type="text" />
+                      <FormControl type="hidden" value="G" />
+                    </FormGroup>
+                  </td>
+                  <td className="tile">
+                    <FormGroup>
+                      <ControlLabel>3</ControlLabel>
+                      <FormControl type="text" />
+                      <FormControl type="hidden" value="N" />
+                    </FormGroup>
+                  </td>
+                  <td className="tile">
+                    <FormGroup>
+                      <ControlLabel>4</ControlLabel>
+                      <FormControl type="text" />
+                      <FormControl type="hidden" value="O" />
+                    </FormGroup>
+                  </td>
+                  <td className="tile">
+                    <FormGroup>
+                      <ControlLabel>5</ControlLabel>
+                      <FormControl type="text" />
+                      <FormControl type="hidden" value="R" />
+                    </FormGroup>
+                  </td>
+                  <td className="tile">
+                    <FormGroup>
+                      <ControlLabel>6</ControlLabel>
+                      <FormControl type="text" />
+                      <FormControl type="hidden" value="A" />
+                    </FormGroup>
+                  </td>
+                  <td className="tile">
+                    <FormGroup>
+                      <ControlLabel>3</ControlLabel>
+                      <FormControl type="text" />
+                      <FormControl type="hidden" value="N" />
+                    </FormGroup>
+                  </td>
+                  <td className="tile">
+                    <FormGroup>
+                      <ControlLabel>7</ControlLabel>
+                      <FormControl type="text" />
+                      <FormControl type="hidden" value="T" />
+                    </FormGroup>
+                  </td>
+                </tr>
+                <tr>
+                  <td colSpan="8" className="text-left">
+                    An act of killing a lot of people.
+                  </td>
+                  <td className="tile">
+                    <FormGroup>
+                      <ControlLabel>2</ControlLabel>
+                      <FormControl type="text" />
+                      <FormControl type="hidden" value="M" />
+                    </FormGroup>
+                  </td>
+                  <td className="tile marked">
+                    <FormGroup>
+                      <FormControl type="text" />
+                      <FormControl type="hidden" value="A" />
+                    </FormGroup>
+                  </td>
+                  <td className="tile">
+                    <FormGroup>
+                      <ControlLabel>8</ControlLabel>
+                      <FormControl type="text" />
+                      <FormControl type="hidden" value="S" />
+                    </FormGroup>
+                  </td>
+                  <td className="tile">
+                    <FormGroup>
+                      <ControlLabel>8</ControlLabel>
+                      <FormControl type="text" />
+                      <FormControl type="hidden" value="S" />
+                    </FormGroup>
+                  </td>
+                  <td className="tile">
+                    <FormGroup>
+                      <ControlLabel>6</ControlLabel>
+                      <FormControl type="text" />
+                      <FormControl type="hidden" value="A" />
+                    </FormGroup>
+                  </td>
+                  <td className="tile">
+                    <FormGroup>
+                      <ControlLabel>9</ControlLabel>
+                      <FormControl type="text" />
+                      <FormControl type="hidden" value="C" />
+                    </FormGroup>
+                  </td>
+                  <td className="tile">
+                    <FormGroup>
+                      <ControlLabel>5</ControlLabel>
+                      <FormControl type="text" />
+                      <FormControl type="hidden" value="R" />
+                    </FormGroup>
+                  </td>
+                  <td className="tile">
+                    <FormGroup>
+                      <ControlLabel>10</ControlLabel>
+                      <FormControl type="text" />
+                      <FormControl type="hidden" value="E" />
+                    </FormGroup>
+                  </td>
+                </tr>
+                <tr>
+                  <td colSpan="8" className="text-left">
+                    A strong wish to achieve something.
+                  </td>
+                  <td>A</td>
+                  <td className="marked">M</td>
+                  <td>B</td>
+                  <td>I</td>
+                  <td>T</td>
+                  <td>I</td>
+                  <td>O</td>
+                  <td>N</td>
+                </tr>
+                <tr>
+                  <td colSpan="8" className="text-left">
+                    The act of going against loyalty.
+                  </td>
+                  <td>B</td>
+                  <td className="marked">E</td>
+                  <td>T</td>
+                  <td>R</td>
+                  <td>A</td>
+                  <td>Y</td>
+                  <td>A</td>
+                  <td>L</td>
+                </tr>
+                <tr>
+                  <td colSpan="8" className="text-left">
+                    A person who watches what happens but has no active part
+                    in it.
+                  </td>
+                  <td className="marked">O</td>
+                  <td>B</td>
+                  <td>S</td>
+                  <td>E</td>
+                  <td>R</td>
+                  <td>V</td>
+                  <td>E</td>
+                  <td>R</td>
+                </tr>
+                <tr>
+                  <td colSpan="8" className="text-left">Frightening.</td>
+                  <td className="marked">F</td>
+                  <td>E</td>
+                  <td>A</td>
+                  <td>R</td>
+                  <td>S</td>
+                  <td>O</td>
+                  <td>M</td>
+                  <td>E</td>
+                </tr>
+                <tr>
+                  <td colSpan="8" className="text-left">
+                    A detailed plan for achieving success in situations such as
+                    war, politics, business, industry, or sport.
+                  </td>
+                  <td>S</td>
+                  <td className="marked">T</td>
+                  <td>R</td>
+                  <td>A</td>
+                  <td>T</td>
+                  <td>E</td>
+                  <td>G</td>
+                  <td>Y</td>
+                </tr>
+                <tr>
+                  <td colSpan="8" className="text-left">
+                    The crime of stealing things.
+                  </td>
+                  <td>T</td>
+                  <td className="marked">H</td>
+                  <td>I</td>
+                  <td>E</td>
+                  <td>V</td>
+                  <td>E</td>
+                  <td>R</td>
+                  <td>Y</td>
+                </tr>
+                <tr>
+                  <td colSpan="8" className="text-left">
+                    A royal title for a female heir to the throne.
+                  </td>
+                  <td>P</td>
+                  <td className="marked">R</td>
+                  <td>I</td>
+                  <td>N</td>
+                  <td>C</td>
+                  <td>E</td>
+                  <td>S</td>
+                  <td>S</td>
+                </tr>
+                <tr>
+                  <td colSpan="8" className="text-left">
+                    To have control over a place or person.
+                  </td>
+                  <td>D</td>
+                  <td className="marked">O</td>
+                  <td>M</td>
+                  <td>I</td>
+                  <td>N</td>
+                  <td>A</td>
+                  <td>T</td>
+                  <td>E</td>
+                </tr>
+                <tr>
+                  <td colSpan="8" className="text-left">
+                    A flat, usually square or rectangular, paper container
+                    for a letter.
+                  </td>
+                  <td>E</td>
+                  <td className="marked">N</td>
+                  <td>V</td>
+                  <td>E</td>
+                  <td>L</td>
+                  <td>O</td>
+                  <td>P</td>
+                  <td>E</td>
+                </tr>
+                <tr>
+                  <td colSpan="8" className="text-left">
+                    A set of formal acts, often fixed and traditional,
+                    performed on important social or religious occasions.
+                  </td>
+                  <td>C</td>
+                  <td className="marked">E</td>
+                  <td>R</td>
+                  <td>E</td>
+                  <td>M</td>
+                  <td>O</td>
+                  <td>N</td>
+                  <td>Y</td>
+                </tr>
+                <tr>
+                  <td colSpan="8" className="text-left">
+                    Accompanied by someone to a specific place.
+                  </td>
+                  <td>E</td>
+                  <td className="marked">S</td>
+                  <td>C</td>
+                  <td>O</td>
+                  <td>R</td>
+                  <td>T</td>
+                  <td>E</td>
+                  <td>D</td>
+                </tr>
+              </tbody>
+            </Table>
+          </Panel>
         </Col>
       </Row>
     );
